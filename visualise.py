206037f87bbd673c8492b297428d2d7f8759cde8
archester-250 <1146802625@qq.com>
2022-04-08 20:35:43 +0800

    finish visualising

--- visualise.py
+++ visualise.py
@@ -1,8 +1,8 @@
 from graphviz import Digraph
 
 
-def visualise(DFA):
-    f = Digraph('finite_state_machine', filename='fsm.gv')
+def visualiseD(DFA):
+    f = Digraph('DFA', filename='DFA.gv')
 
     f.attr(rankdir='LR', size='8,5')
 
@@ -16,5 +16,26 @@ def visualise(DFA):
     f.attr('node', shape='none')
     for i in DFA.startstates:
         f.edge('', str(i).replace('\'', ''))
+
+    f.view()
+
+
+def visualiseN(NFA):
+    f = Digraph('NFA', filename='NFA.gv')
+
+    f.attr(rankdir='LR', size='8,5')
+
+    f.attr('node', shape='doublecircle')
+    for i in NFA.endstates:
+        f.node(str(i).replace('\'', ''))
+
+    f.attr('node', shape='circle')
+    for i in NFA.trans:
+        for j in i.end:
+            if(j != 'NoS'):
+                f.edge(str(i.start).replace('\'', ''), str(j).replace('\'', ''), label=str(i.cond))
+    f.attr('node', shape='none')
+    for i in NFA.startstates:
+        f.edge('', str(i[0]).replace('\'', ''))
 
     f.view()
